e63eb4972ba7ef8a15e73c4208ec35d1d4bc932d
ZhenyuWang <zhenyu.wang1@eeoa.com>
2023-06-20 20:20:07 +0800

    fix: 改用electron-builder打包，成功更换图标

--- forge.config.js
+++ forge.config.js
@@ -6,24 +6,43 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: (arch) => ({
+        name: 'electron-fiddle',
+        authors: 'Electron Community',
+        exe: 'electron-fiddle.exe',
+        iconUrl:
+          'https://raw.githubusercontent.com/electron/fiddle/0119f0ce697f5ff7dec4fe51f17620c78cfd488b/assets/icons/fiddle.ico',
+        noMsi: true,
+        setupExe: `electron-fiddle-${version}-win32-${arch}-setup.exe`,
+        setupIcon: 'fiddle.png',
+      }),
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+      config: {
+        name: '123',
+        icon: "fiddle.png",
+      }
     },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    // {
+    //   name: "@electron-forge/maker-deb",
+    //   config: {
+    //     options: {
+    //       icon: './build/icons/1.icns',
+    //     },
+    //   },
+    // },
+    // {
+    //   name: "@electron-forge/maker-rpm",
+    //   config: {
+    //     icon: "./build/icons/1.icns",
+    //   },
+    // },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
--- main.js
+++ main.js
@@ -8,11 +8,16 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
+    icon: path.join(__dirname, "fiddle.png"),
   });
+  if (process.platform === "darwin") {
+    app.dock.setIcon(path.join(__dirname, "fiddle.png"));
+  }
   win.loadFile("index.html");
-  win.webContents.openDevTools({ mode: "right" });
-//   const contents = win.webContents;
-//   console.log(contents);
+  //   win.loadURL('https://wl.nobook.com/index.html#/guidPost/4100?classId=54&className=NOBOOK-%E6%88%90%E9%95%BF%E8%90%A5&class_no=1655910943157073&grade=2&mapId=888&postId=new')
+  //   win.webContents.openDevTools({ mode: "right" }),
+  //   const contents = win.webContents;
+  //   console.log(contents);
 };
 
 app.whenReady().then(() => {
